83467ff3f5e5e6deb37436ccf455ee2660052815
Lee Sainsbury <Senpuria@live.co.uk>
2015-03-10 18:25:47 +0000

    Master Changelog 0016
    Changelog Revision 0016

---
+++ _wowstack/DB_Master_Changelog.sql
@@ -0,0 +1,214 @@
+-- Revision: 0016
+
+-- Cannoneer Whessan Flee at 15%
+-- Cannoneer Smythe Flee at 15%
+-- Update health for Sleeby
+-- Add AI script for Sleeby
+-- Correct Znort health
+-- Correct Hederine Initiate walk speed
+--  Correct Hederine Manstalker walk speed
+-- Correct Hederine Slayer walk speed
+-- Correct Cliff Thunderer walk speed
+-- Add spell template for Nightbane Shadow Weaver
+-- Add AI script for Nightbane Shadow Weaver
+-- Correct Captain Armando Ossex Rank (Elite)
+-- Add spell template for Old Serra'kis
+-- Add EventAI for Serra'kis
+-- Add AI script for Serra'kis
+-- Correct AI script for Redridge Mongrel
+-- Update AI name and spell template for Sarilus Foulborne
+-- Add AI script for Sarilus Foulborne
+-- Add AI Text for Doctor Theolen Krastinov
+-- Correct creature AI text for Doctor Theolen Krastinov
+-- Correct AI script for Grave Robber
+-- Razrmane Mystic Flee at 15% HP
+-- Baron Longshore Flee at 15% HP
+-- Set Baron Longshore creature AI
+-- Southsea Brigand Flee at 15% HP
+-- Bristleback Thornweaver Flee at 15% HP
+-- Bristleback Water Seeker Flee at 15% HP
+-- Greater Plainstrider - Flee at 15% HP
+-- Tazan - Flee at 15% HP
+-- Ornery Plainstrider - Flee at 15% HP
+-- Razormane Thornweaver - Flee at 15% HP
+-- Razormane Water Seeker - Flee at 15% HP
+-- Razormane Defender - Flee at 15% HP
+-- Kreenig Snarlsnout - Flee at 15% HP
+-- Witchwing Harpy - Flee at 15% HP
+-- Witchwing Ambusher - Flee at 15% HP
+-- Witchwing Roguefeather - Flee at 15% HP
+-- Witchwing Slayer - Flee at 15% HP
+-- Witchwing Windcaller - Flee at 15% HP
+-- Serena Bloodfeather - Flee at 15% HP
+-- AI for Chillwind Ravager
+-- AI for Fledgling Chillwind
+-- Gazzuz summon Voidwalker
+-- AI for Frostsaber Huntress
+-- AI for Frostsaber Huntress
+-- AI for Kurmoikk
+-- Change Timer of Marsh Inkspewer
+-- Correct level of rare Accursed Slitherblade
+-- Update Immune Mark for Razelikh the Defiler
+-- Update Min and Max level for Lumbering Horror (Scourge Invasion)
+-- Remove flee from Deepmoss Venomspitter (Beasts should never flee)
+-- Adjust hitpoints of Mana Sprite
+-- Adjust movement speed of Guard Slip'kik
+-- Slow run speed for Unstable Corpse
+-- Slow run speed for Reanimated Corpse
+-- Creature AI for Pesterhide Snarler
+-- Creature AI for Grimtotem Bandit
+-- Added Silithid Invader for quest
+-- Make Chromatic Elite Guard not immune to fear
+-- Correct health for King Magni Bronzebeard
+-- Correct health for Cairne Bloodhoof
+-- Correct health for High Tinker Mekkatorque
+--Sunscale Screecher - Call for help at 15% HP
+-- Southsea Cutthroat - Flee at 15% HP
+-- Zalazane - Flee at 15% HP
+-- Voodoo Troll - Flee at 15% HP
+-- Correct Zzarc' Vul level
+-- Correct level for Pimgib
+-- Correct stats for General Marcus Jonathan
+-- Correct Afrasiabi health
+-- Correct Major Mattingly health
+-- Fix Blood Steward of Kirtonos walk speed
+-- Fix Rat health
+-- Correct Unstable Corpse AI
+-- Correct walk speed for Princess Theradras
+-- Fix Adder health
+-- Correct resistances for Lord Roccor
+-- Correct health for Krellack
+-- Correct creature template stats for Irondeep Surveyor
+-- Correct Frostwolf Bowman health
+-- Correct Stormpike Guardsman health
+-- Correct Frostwolf Legionnaire health
+-- Correct Stormpike Guardsman health
+-- Correct Frostwolf Guardian health
+-- Correct Coldmine Invader level
+-- Add AI script for Twilight Lord Everun
+-- Remove loot template for Ghost of the Past
+-- Update Immunities for Ironbark Protector
+-- Set stun immunity for the following creatures
+-- Revelosh
+-- Ironaya
+-- Ancient Stone Keeper
+-- Galgann Firehammer
+-- Grimlok
+-- Archaedas
+-- Noxxion
+-- Lord Vyletongue
+-- Celebras the Cursed
+-- Landslide
+-- Princess Theradras
+-- Rotgrip
+-- Tinkerer Gizlock
+-- Antu'sul
+-- Theka the Martyr
+-- Witch Doctor Zum'rah
+-- Chief Ukorz Sandscalp
+-- Ruuzlu
+-- Gahz'rilla
+-- Houndmaster Loksey
+-- Arcanist Doan
+-- Herod
+-- High Inquisitor Fairbanks
+-- Scarlet Commander Mograine
+-- High Inquisitor Whitemane
+-- Azshir the Sleepless
+-- Remains of a paladin Dynamic Flags
+-- Adjust speedwalk of Cave Lurker
+-- Correct position for William MacGregor
+-- Update position for Bodyguard Bartrand
+-- Correct movement for Lok Orcbane
+-- Correct AI for Lok Orcbane
+-- Correct Razormane Seer AI
+-- Correct Razormane Stalker AI
+-- Update position for Warleader Krazzilak
+-- Prevent Blood Elf Reclaimer from fleeing
+-- Correct Greater Thunderhawk Thunderclap initial timer
+-- Correct AI for Blood Elf Surveyor and prevent it from fleeing
+-- Add AI for Captain Gerogg Hammertoe
+-- Update Immune Mask for Hagg Taurenbane
+-- Hagg Taurenbane Flee at 15% HP
+-- Greater Plainstrider Flee at 15% HP
+-- Add AI for Jaguero Stalker
+-- Silithid Swarmer cast Silithid Swarm on Spawn
+-- Stop deeprun Rat from instantly respawning
+-- Tinkerer Sniggles flee at 15% HP
+-- Venture Co. Mercenary Flee at 15% HP
+-- Venture Co. Peon Flee at 15% HP
+-- Add AI text for Kul Tiras Sailor
+-- Mist Howler Call for Help at 50% HP
+-- Venture Co. Enforcer Flee at 15% HP
+-- Add Gossip for Remy "Two Times"
+-- Correct Position for Marez Cowl
+-- Fleeting Plainstrider Flee at 15% HP
+-- Bael'dun Officer Flee at 15% HP
+-- Bael'dun Soldier Flee at 15% HP
+-- Gogger Rock Keeper Shound't Run
+-- Gogger Stonepounder Shouldn't Run
+-- Add AI for Sarilus Foulborne
+-- Add Well Fed for Winter Veil Cookie
+-- Correct Stats for Shriveled Heart
+-- Correct Charges for Hallowed Wand - Random
+-- Correct Charges for Hallowed Wand - Pirate
+-- Fix Drop Rate for Magic Dust
+-- Stop Crystalline Threaded Cape dropping from creatures
+-- Correct loot template foir Shadowhide Slayer
+-- Correct loot template foir Rabid Shadowhide Gnoll
+-- Correct loot template foir Shadowhide Gnoll
+-- Correct Attack Speed for Atiesh, Greatstaff of the Guardian
+-- Correct Loot template for Murloc Warrior
+-- Correct drop chance for Recipe: Elixir of the Sages
+-- Correct drop chance for Recipe: Elixir of the Mongoose
+-- Correct loot template for Princess Theradras
+-- Correct loot template for Shade of Eranikus
+-- Stop totems from being able to parry
+-- Stop totems from being able to parry
+-- Correct minimum level & reputation  for A Warden of the Alliance
+-- Correct minimum level & reputation for You Scream, I Scream...
+-- Correct minimum level & reputation for Jaina's Autograph
+-- Correct minimum level & reputation for The Stonewrought Dam
+-- Correct minumum level & reputation for The Bough of the Eternals
+-- Correct minumum level & reputation for Spooky Lighthouse
+-- Correct minumum level & reputation for A Warden of the Horde
+-- Correct minumum level & reputation for Cairne's Hoofprint
+-- Correct minumum level & reputation for You Scream, I Scream...
+-- Correct minumum level & reputation for Gateway to the Frontier
+-- Correct minumum level & reputation for Down at the Docks
+-- Correct reputation for Children's Week (Alliance)
+-- Correct reputation for Children's Week (Horde)
+-- Correct quest chain for The Disruption Ends
+-- Correct quest chain for Supplies for the Crossroads
+-- Correct quest chain for Volunteer's Battlegear
+-- Correct quest chain for Champion's Battlegear
+-- Correct quest chain for Veteran's Battlegear
+-- Update creature_template SpeedRun & SpeedWalk
+-- Defusing the Threat removed in 1.10
+-- Correct quest level for The Super Egg-O-Matic
+-- Fix drop chance of Glutton Shackle and Darksoul Shackle for Arugal's Folly
+-- Correct reputation reward for Troll Necklace Bounty
+-- Correct minimum level for Hidden Enemies
+-- Correct prerequisite for Proof of Demise
+-- Make Fool's Stout repeatable
+-- Correct race requirement for A New Plague
+-- Correct race requirement for Your Place In The World
+-- Unchain Rude Awakening & The Mindless Ones
+-- Correct chain for Report to Orgnil
+-- Correct reputation for Missing Reputation
+-- Correct quest level for Never Ask Me About My Business
+-- Corrected 23 Quest Levels
+-- Correct minimum level requirement for A Gently Shaken Gift
+-- Correct minimum level requirement for Greatfather Winter is Here! (Alliance)
+-- Correct minimum level requirement for Greatfather Winter is Here! (Horde)
+-- Correct reputation for One Commendation Signet
+-- Make Thirty Signets for War Supplies repeatable
+-- Correct reputation for Ten Commendation Signets
+-- Remove Korrak the Bloodrager
+-- Remove Korrak the Everliving
+-- Bloodfury Harpy Flee at 15% HP
+--  Bloodfury Windcaller Flee at 15% HP
+-- Bloodfury Storm Witch Flee at 15% HP
+-- Bloodfury Slayer Flee at 15% HP
+-- Bloodfury Roguefeather Flee at 15% HP
+-- Razormane Warfrenzy Flee at 15% HP
