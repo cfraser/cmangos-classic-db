7e3f2fa0f2c97f97be7e6c58606e1ebea52259c1
Lee Sainsbury <Senpuria@live.co.uk>
2015-03-15 16:03:26 +0000

    Revert "Fix 0019"
    This reverts commit 98f66bc5796e8ec092179585c586a5763f066e99.

--- _wowstack/0019_wowstack_Creature_Updates.sql
+++ _wowstack/0019_wowstack_Creature_Updates.sql
@@ -1,2 +1,2 @@
 -- Brontus now walks at the correct speed as has his shiny rare elite badge
-UPDATE `creature_template` SET `SpeedWalk`='1', `Rank`='4' WHERE `Entry`='5827';
+UPDATE `creature_template` SET `SpeedWalk`='1', `Rank`='2' WHERE `Entry`='5827';
